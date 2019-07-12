55dc654f1f2a1efd1e4721f5cd964e7e95854974
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 01:47:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,58 @@
-# My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+    i = 0
+  while source_array[i] do
+    new_array.push(source_array[i] * -1)
+      i += 1
+    end
+    return new_array
+  end
+  
+def map_to_no_change(source_array)
+  new_array = []
+    i = 0
+  while source_array[i] do
+    new_array.push(source_array[i])
+    i += 1
+  end
+  return new_array
+end
+  
+def map_to_double(source_array)
+    new_array = []
+    i = 0 
+    while source_array[i] do
+    new_array.push(source_array[i] * 2)
+    i += 1
+    end  
+  return new_array
+end
+  
+def map_to_square(source_array)
+  new_array = []
+  i = 0
+  while source_array[i] do
+  new_array.push(source_array[i] % 2)
+  i =+ 1
+  end 
+  return new_array
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
